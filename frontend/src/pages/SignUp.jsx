45e3f8bb669cb0d00999b32b05ed947331aa3606
anusha0507 <anushasahu0507@gmail.com>
2024-05-03 13:12:52 +0530

    added experts, created contact page and fixed minor mistakes

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-// import React from 'react'
-// import Signup from '../assets/Signup.jpg'
-=======
 import React from 'react'
 import { Form, Input, Radio, message} from 'antd';
 import '../styles/SignUp.css'
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
->>>>>>> d29de792e7b305676811f6faff6e74ade0ce35e1
 
 // const SignUp = () => {
 //   return (
@@ -47,8 +42,6 @@ import { useState } from 'react';
 //   )
 // }
 
-// export default SignUp;
-=======
 const SignUp = () => {
   const [form] = Form.useForm();
   const [formData, setFormData] = useState({
@@ -83,36 +76,8 @@ const SignUp = () => {
     }
     
   };
->>>>>>> d29de792e7b305676811f6faff6e74ade0ce35e1
 
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-<<<<<<< HEAD
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-=======
         <Form form={form} layout='horizontal' onFinish={onfinishHandler} 
         className='signup-form'>
 
